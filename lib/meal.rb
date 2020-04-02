4a30da0a35dc93077e71aec0bba5d7d7de76c235
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-02 06:12:16 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -6,7 +6,10 @@ class Meal
 
 
   def initialize(waiter, customer, total, tip=0)
-    @waiter, @customer, @total, @tip = waiter, customer, total, tip
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
     @@all << self
   end
 
