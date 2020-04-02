335eb814a700856472bee9a75da317416b0eec6b
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-02 05:26:40 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
 
+  @@all = []
+
+  attr_accessor :name, :years_experience
+
+
+  def intialize(name, years_experience)
+    @name, @years_experience - name, years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  
+
 end
